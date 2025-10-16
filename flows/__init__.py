63e2a37ba014f56d765a707987bb7bf32dd9c093
Alan Gunning <alangunning@gmail.com>
2025-10-16 17:23:09 +0100

    Add Prefect similarity scan flow and reporting

--- flows/__init__.py
+++ flows/__init__.py
@@ -2,10 +2,12 @@
 
 from .backtest_flow import scheduled_backtest_runner
 from .embedding_flow import supabase_embedding_refresh
+from .similarity_scans import similarity_scan_flow
 from .prune_flow import scheduled_vector_prune
 
 __all__ = [
     "scheduled_backtest_runner",
     "scheduled_vector_prune",
     "supabase_embedding_refresh",
+    "similarity_scan_flow",
 ]
